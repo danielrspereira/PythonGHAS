8e6c7e3928199a80252bfdcf5a695ca0330bdc0d
Daniel Pereira <danielrspereira@hotmail.com>
2024-07-21 17:37:42 +0200

    first commit

---
+++ db.py
@@ -0,0 +1,6 @@
+class connect_to_db:
+    def __init__(self, host, user, password, db_name):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db_name = db_name
---
+++ main.py
@@ -0,0 +1,4 @@
+import db
+
+print("Hello World")
+db = db.connect_to_db("server123@company.org", "maxmustermann", "max123", "cad_data")
